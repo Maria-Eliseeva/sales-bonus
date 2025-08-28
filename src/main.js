2cb9a1b7a10e273778445473920f939564c6e3f3
Maria-Eliseeva <marymaryeliseeva@yandex.ru>
2025-08-28 23:09:42 +0300

    check__arr

--- src/main.js
+++ src/main.js
@@ -49,6 +49,7 @@ function analyzeSalesData(data, options) {
     || data.sellers.length === 0
     || data.products.length  === 0
     || data.customers.length === 0
+    || data.purchase_records.length === 0
 ) {
     throw new Error('Некорректные входные данные');
 }
